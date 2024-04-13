e68e17fcbbc671934fd8a99c52c974daf2d06c58
Gabriel da Silveira Miranda <gabrielsmbr@gmail.com>
2024-04-13 15:19:00 -0300

    Put e Delete implementado

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from .models import TodoList
+from .models import TodoList, Todo
 from .serializers import TodoSerializer, TodoListSerializer
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -37,31 +37,56 @@ def allTodos(req):
 @permission_classes({IsAuthenticated}) #verifica se o usuário está autorizado
 def CreateTodoList(req):
     if req.method == 'POST':  #cria uma nova lista
-        if not req.user.is_authenticated: #verifica se o usuário está autorizado
-            return Response({'message': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
         serializer = TodoListSerializer(data=req.data)
         if serializer.is_valid():
+            todo_list_title = serializer.validated_data.get('title')
+            if TodoList.objects.filter(title=todo_list_title, user=req.user).exists():
+                return Response({'error': 'Todo list with this title already exists'}, status=status.HTTP_400_BAD_REQUEST)
             serializer.validated_data['user'] = req.user
             serializer.save()
-            return Response(status=status.HTTP_201_CREATED) #Retorna o status 201
+            return Response({'message' : 'Todo List created'}, status=status.HTTP_201_CREATED) #Retorna o status 201
         return Response(status=status.HTTP_400_BAD_REQUEST) #Retorna bad request caso o serializer seja inválido
  
-@api_view(['POST'])
+@api_view(['POST', 'DELETE', 'PUT'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes({IsAuthenticated}) #Cria um ToDo e adciona á uma todoList
 def CreateTodo(req):
-    if not req.user.is_authenticated: #Verifica se o usuário está autenticado
-        return Response({'message': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
-    if 'todo_list_title' not in req.data: #Procura no request o nome da TodoList
-         return Response({'error': 'Todo list title is required'}, status=status.HTTP_400_BAD_REQUEST)
-    todo_list_title = req.data['todo_list_title'] #Pega a Todolist pai
-    try: 
-        todo_list = TodoList.objects.get(title=todo_list_title, user=req.user)
-    except TodoList.DoesNotExist:
-        return Response({'message': 'Todo list not found'}, status=status.HTTP_404_NOT_FOUND)
-    req.data['todoList'] = todo_list.id  # Adiciona o título da lista todo ao dados do todo antes de serializá-lo 
-    serializer = TodoSerializer(data=req.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if req.method == 'POST':
+        if 'todo_list_title' not in req.data: #Procura no request o nome da TodoList
+            return Response({'error': 'Todo list title is required'}, status=status.HTTP_400_BAD_REQUEST)
+        todo_list_title = req.data['todo_list_title'] #Pega a Todolist pai
+        try: 
+            todo_list = TodoList.objects.get(title=todo_list_title, user=req.user)
+        except TodoList.DoesNotExist:
+            return Response({'message': 'Todo list not found'}, status=status.HTTP_404_NOT_FOUND)
+        req.data['todoList'] = todo_list.id  # Adiciona o título da lista todo ao dados do todo antes de serializá-lo 
+        serializer = TodoSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif req.method == 'DELETE':
+        if 'todo_id' not in req.data:
+            return Response({"message" : "Todo ID é exigido."}, status=status.HTTP_400_BAD_REQUEST)
+        todo_id = req.data['todo_id']
+        try:
+            todo = Todo.objects.get(id=todo_id)
+        except Todo.DoesNotExist:
+            return Response({'message': 'Todo not found'}, status=status.HTTP_404_NOT_FOUND)   
+        todo.delete()  # Exclua o ToDo
+        return Response({'message': 'Todo deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    
+    elif req.method == 'PUT':
+        if 'todo_id' not in req.data:
+            return Response({"message" : "Todo ID é exigido."}, status=status.HTTP_400_BAD_REQUEST)
+        todo_id = req.data['todo_id']
+        try:
+            todo = Todo.objects.get(id=todo_id)
+        except Todo.DoesNotExist:
+            return Response({'message': 'Todo not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TodoSerializer(todo, data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
